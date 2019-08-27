abb94bc98c062b2b52a0cd33b4cf2276714801b1
GiGong <gigong222@gmail.com>
2019-08-27 17:35:06 +0900

    Develop MainWindow, VM

--- Memories/ViewModels/MainWindowVM.cs
+++ Memories/ViewModels/MainWindowVM.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Memories.Views;
+using Prism.Commands;
 
 namespace Memories.ViewModels
 {
     public class MainWindowVM
     {
+        #region Field
+
+        private DelegateCommand _newBookCommand;
+        private DelegateCommand _loadBookCommand;
+
+        #endregion
+
+        #region Command
+
+        public DelegateCommand NewBookCommand =>
+            _newBookCommand ?? (_newBookCommand = new DelegateCommand(NewBook));
+
+        public DelegateCommand LoadBookCommand =>
+            _loadBookCommand ?? (_loadBookCommand = new DelegateCommand(LoadBook));
+
+        #endregion Command
+
+        #region Method
+
+        private void NewBook()
+        {
+            new NewBookWindow().Show();
+        }
+
+        void LoadBook()
+        {
+
+        }
+
+        #endregion Method
+
     }
 }
