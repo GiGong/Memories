542df4c876cd6f1e60ce96572dd419fbaa0e9a4b
GiGong <gigong222@gmail.com>
2019-08-27 19:23:16 +0900

    Develop InputBookInfoView, VM.

--- Memories/ViewModels/InputBookInfoViewVM.cs
+++ Memories/ViewModels/InputBookInfoViewVM.cs
@@ -1,12 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using Prism.Commands;
+using Prism.Mvvm;
 
 namespace Memories.ViewModels
 {
-    public class InputBookInfoViewVM
+    public class InputBookInfoViewVM : BindableBase
     {
+        #region Field
+
+        private string _bookTitle;
+        private string _writer;
+        private string _filePath;
+
+        private DelegateCommand _selectFilePathCommand;
+
+        #endregion Field
+
+        #region Property
+
+        public string Booktitle
+        {
+            get { return _bookTitle; }
+            set { SetProperty(ref _bookTitle, value); }
+        }
+
+        public string Writer
+        {
+            get { return _writer; }
+            set { SetProperty(ref _writer, value); }
+        }
+
+        public string FilePath
+        {
+            get { return _filePath; }
+            set { SetProperty(ref _filePath, value); }
+        }
+
+        #endregion Property
+
+        #region Command
+
+        public DelegateCommand SelectFilePathCommand =>
+            _selectFilePathCommand ?? (_selectFilePathCommand = new DelegateCommand(SelectFilePath));
+
+        #endregion Command
+
+        #region Method
+
+        private void SelectFilePath()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                FilePath = saveFileDialog.FileName;
+            }
+        } 
+
+        #endregion Method
     }
 }
