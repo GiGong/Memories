5a1c4b7eddeeb27d3d51ae95110c2812bc1ec351
GiGong <gigong222@gmail.com>
2019-08-30 16:54:41 +0900

    Add Int to Visibility Converter.

---
+++ Memories/Converters/IntToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Memories.Converters
+{
+    public class IntToVisibilityConverter : IValueConverter
+    {
+        private int val;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is int))
+            {
+                throw new ApplicationException("Value has to be of type '" + typeof(int).FullName + "'!");
+            }
+
+            int intParam = (int)parameter;
+            val = (int)value;
+
+            return ((intParam & val) != 0) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
