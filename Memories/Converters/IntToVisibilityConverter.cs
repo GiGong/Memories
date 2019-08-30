20702bc8015ea908fb6b7ef2e384494b0e9122f1
GiGong <gigong222@gmail.com>
2019-08-30 17:33:45 +0900

    Fix Int to Visibility Converter. Can check if value, parameter are String.

--- Memories/Converters/IntToVisibilityConverter.cs
+++ Memories/Converters/IntToVisibilityConverter.cs
@@ -7,23 +7,27 @@ namespace Memories.Converters
 {
     public class IntToVisibilityConverter : IValueConverter
     {
-        private int val;
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (!(value is int))
+            if (!(value is int) && !int.TryParse(value.ToString(), out _))
             {
                 throw new ApplicationException("Value has to be of type '" + typeof(int).FullName + "'!");
             }
 
-            int intParam = (int)parameter;
-            val = (int)value;
+            if (!(parameter is int) && !int.TryParse(parameter.ToString(), out _))
+            {
+                throw new ApplicationException("Parameter has to be of type '" + typeof(int).FullName + "'!");
+            }
+
+            int intParam = System.Convert.ToInt32(parameter);
+            int val = System.Convert.ToInt32(value);
 
             return ((intParam & val) != 0) ? Visibility.Visible : Visibility.Collapsed;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return null;
+            throw new NotSupportedException("This converter can only convert!");
         }
     }
 }
