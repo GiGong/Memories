8d3206fee2e77789ec64f08e0b02a1540fe0aa00
GiGong <gigong222@gmail.com>
2019-09-02 18:04:26 +0900

    Add LayoutSelectView.

---
+++ Memories/Views/LayoutSelectView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Memories.Views
+{
+    /// <summary>
+    /// Interaction logic for LayoutPreviewView
+    /// </summary>
+    public partial class LayoutSelectView : UserControl
+    {
+        public LayoutSelectView()
+        {
+            InitializeComponent();
+        }
+    }
+}
