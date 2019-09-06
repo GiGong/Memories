c261d00711e5ebb172374dee9a779ac3ed2fecd1
GiGong <gigong222@gmail.com>
2019-09-06 18:33:43 +0900

    Add Enum Description Converter for form shown. Add Description to PaperSize Enum.

--- Memories/Enums/PaperSize.cs
+++ Memories/Enums/PaperSize.cs
@@ -1,14 +1,26 @@
-﻿namespace Memories.Enums
+﻿using Memories.Converters;
+using System.ComponentModel;
+
+namespace Memories.Enums
 {
+    [TypeConverter(typeof(EnumDescriptionTypeConverter))]
     public enum PaperSize
     {
+        [Description("A3 : 420 * 297")]
         A3,
+        [Description("A4 : 297 * 210")]
         A4,
+        [Description("A5 : 210 * 148")]
         A5,
+        [Description("A6 : 148 * 105")]
         A6,
+        [Description("B4 : 374 * 254")]
         B4,
+        [Description("B5 : 257 * 188")]
         B5,
+        [Description("B6 : 188 * 128")]
         B6,
+        [Description("비규격 : 사용자 설정")]
         비규격
     }
 }
