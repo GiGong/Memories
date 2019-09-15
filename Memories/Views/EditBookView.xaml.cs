5cdb9a7b1542f0a4541a52bce1a9fefed70d4009
GiGong <gigong222@gmail.com>
2019-09-16 02:04:58 +0900

    Change Window to View for MVVM.

---
+++ Memories/Views/EditBookView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Memories.Views
+{
+    /// <summary>
+    /// Interaction logic for EditBookView.xaml
+    /// </summary>
+    public partial class EditBookView : UserControl
+    {
+        public EditBookView()
+        {
+            InitializeComponent();
+        }
+    }
+}
