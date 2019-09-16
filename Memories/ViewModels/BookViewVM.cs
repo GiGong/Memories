a3020fa965c0a4253b16d4ccedddb7b271d70d0b
GiGong <gigong222@gmail.com>
2019-09-16 19:39:46 +0900

    Use Region for BookPageView.

--- Memories/ViewModels/BookViewVM.cs
+++ Memories/ViewModels/BookViewVM.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Memories.Views;
+using Prism.Regions;
 
 namespace Memories.ViewModels
 {
     public class BookViewVM
     {
+        public BookViewVM(IRegionManager regionManager)
+        {
+            regionManager.RegisterViewWithRegion("BookLeftRegion", typeof(BookPageView));
+            regionManager.RegisterViewWithRegion("BookRightRegion", typeof(BookPageView));
+        }
     }
 }
