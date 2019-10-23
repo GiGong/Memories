968494834af46f3fa96a6eaa265cf52261379023
GiGong <gigong222@gmail.com>
2019-10-23 22:21:01 +0900

    Simple design using Mahapps.Metro.

--- Memories/Memories.Core/Controls/MMDialogWindow.xaml.cs
+++ Memories/Memories.Core/Controls/MMDialogWindow.xaml.cs
@@ -1,12 +1,11 @@
 ﻿using Prism.Services.Dialogs;
-using System.Windows;
 
 namespace Memories.Core.Controls
 {
     /// <summary>
     /// Interaction logic for MMDialogWindow.xaml
     /// </summary>
-    public partial class MMDialogWindow : Window, IDialogWindow
+    public partial class MMDialogWindow : IDialogWindow
     {
         public IDialogResult Result { get; set; }
 
--- Memories/Memories/Views/MainWindow.xaml.cs
+++ Memories/Memories/Views/MainWindow.xaml.cs
@@ -1,11 +1,9 @@
-﻿using System.Windows;
-
-namespace Memories.Views
+﻿namespace Memories.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
